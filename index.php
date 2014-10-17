6f328f15ca4256b2db165ec01abe6cc771a73864
ecp <emmanuel.coppey@gmail.com>
2014-10-17 19:39:04 +0200

    Bases de l'accueil
    Formulaire d’itinéraire

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Autocar</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<div class="intro">
+		<h1>Centrocar, le renouveau de l'autobus</h1>
+		<form>
+			<input type="text" placeholder="Je pars">
+			<input type="text" placeholder="J'arrive">
+		</form>
+	</div>
+</body>
+</html>
