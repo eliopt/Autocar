f9280b14e0aa91a19308ded5beba898d6d66debe
ecp <emmanuel.coppey@gmail.com>
2014-10-18 20:18:04 +0200

    Adresse verification avant debut itinéraire

---
+++ js/autocar.js
@@ -0,0 +1,31 @@
+function introFormVerif() {
+  if($('#depart').val() && $('#arrivee').val()) {
+    var geocoder = new google.maps.Geocoder();
+    var ok = 0;
+    geocoder.geocode( { 'address': $('#depart').val()}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        ok++;
+        departLocation = results[0].geometry.location;
+        if(ok == 2) itineraire(departLocation, arriveeLocation);
+      } else {
+        alert('Erreur, l\'adresse n\'est pas valide');
+      }
+    });
+    geocoder.geocode( { 'address': $('#arrivee').val()}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        ok++;
+        arriveeLocation = results[0].geometry.location;
+        if(ok == 2) itineraire(departLocation, arriveeLocation);
+      } else {
+        alert('Erreur, l\'adresse n\'est pas valide');
+      }
+    });
+    var departPremiereValeur = $('#depart').val();
+    var arriveePremiereValeur = $('#arrivee').val();
+  } else {
+    alert('Erreur, les champs sont vides');
+  }
+}
+function itineraire(departLocation, arriveeLocation) {
+  alert(departLocation+arriveeLocation);
+}
