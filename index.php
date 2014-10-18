6fdf80f39fdb6f9f813bf71d50d70dd28eb7a7e4
ecp <emmanuel.coppey@gmail.com>
2014-10-18 20:17:36 +0200

    Adresse autocomplete

--- index.php
+++ index.php
@@ -3,15 +3,33 @@
 <head>
   <meta charset="utf-8">
   <title>Autocar</title>
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places"></script>
+  <script src="./js/jquery.geocomplete.js"></script>
+  <script src="./js/autocar.js"></script>
   <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 	<div class="intro">
 		<h1>Centrocar, le renouveau de l'autobus</h1>
-		<form>
-			<input type="text" placeholder="Je pars">
-			<input type="text" placeholder="J'arrive">
+		<form onSubmit="introFormVerif();">
+			<div class="introItineraire">
+				<div class="introItemContainer">
+					<div class="introDepart"></div>
+				</div>
+				<div class="introItemContainer">
+					<div class="introArrivee"></div>
+				</div>
+			</div>
+			<div class="introInput">
+				<input type="text" id="depart" placeholder="Départ">
+				<input type="text" id="arrivee" placeholder="Arrivée">
+			</div>
+			<input type="submit" onClick="introFormVerif();return false;" value="C'est parti !">
 		</form>
 	</div>
+	<script type="text/javascript">
+	$("input").geocomplete();
+	</script>
 </body>
 </html>
