27f8ff2af9d7d6b831579238558fadc6c191ebbe
ecp <emmanuel.coppey@gmail.com>
2014-10-26 23:13:50 +0100

    Verification des données de l'itinéraire

--- loadItineraire.php
+++ loadItineraire.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
-if(preg_match('#\([0-9\.]+,[0-9\.]+\)#', $_POST['de'])) {
-
+if(preg_match('#\([0-9\.]+, [0-9\.]+\)#', $_POST['de']) AND preg_match('#\([0-9\.]+, [0-9\.]+\)#', $_POST['a'])) {
+	$de = preg_replace('#[\(\) ]#', '', $_POST['de']);
+	$de = preg_split('#,#', $de);
+	$_SESSION['de'] = $de;
+	$a = preg_replace('#[\(\) ]#', '', $_POST['a']);
+	$a = preg_split('#,#', $a);
+	$_SESSION['a'] = $a;
+	echo 'Success';
+} else {
+	echo 'Error';
 }
-$_SESSION['de'] = 
 ?>
