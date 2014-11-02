e631fa978eb096c0542e10290eefac41da37ee74
eliopt <emmanuel.coppey@gmail.com>
2014-11-02 13:03:36 +0100

    Ajout de dijkstra pour le calcul d'itinéraire
    Reste à vérifier chaque possibilité de dates et de places.

--- index.js
+++ index.js
@@ -1,3 +1,149 @@
+var Graph = (function (undefined) {
+
+	var extractKeys = function (obj) {
+		var keys = [], key;
+		for (key in obj) {
+		    Object.prototype.hasOwnProperty.call(obj,key) && keys.push(key);
+		}
+		return keys;
+	}
+
+	var sorter = function (a, b) {
+		return parseFloat (a) - parseFloat (b);
+	}
+
+	var findPaths = function (map, start, end, infinity) {
+		infinity = infinity || Infinity;
+
+		var costs = {},
+		    open = {'0': [start]},
+		    predecessors = {},
+		    keys;
+
+		var addToOpen = function (cost, vertex) {
+			var key = "" + cost;
+			if (!open[key]) open[key] = [];
+			open[key].push(vertex);
+		}
+
+		costs[start] = 0;
+
+		while (open) {
+			if(!(keys = extractKeys(open)).length) break;
+
+			keys.sort(sorter);
+
+			var key = keys[0],
+			    bucket = open[key],
+			    node = bucket.shift(),
+			    currentCost = parseFloat(key),
+			    adjacentNodes = map[node] || {};
+
+			if (!bucket.length) delete open[key];
+
+			for (var vertex in adjacentNodes) {
+			    if (Object.prototype.hasOwnProperty.call(adjacentNodes, vertex)) {
+					var cost = adjacentNodes[vertex],
+					    totalCost = cost + currentCost,
+					    vertexCost = costs[vertex];
+
+					if ((vertexCost === undefined) || (vertexCost > totalCost)) {
+						costs[vertex] = totalCost;
+						addToOpen(totalCost, vertex);
+						predecessors[vertex] = node;
+					}
+				}
+			}
+		}
+
+		if (costs[end] === undefined) {
+			return null;
+		} else {
+			return predecessors;
+		}
+
+	}
+
+	var extractShortest = function (predecessors, end) {
+		var nodes = [],
+		    u = end;
+
+		while (u) {
+			nodes.push(u);
+			predecessor = predecessors[u];
+			u = predecessors[u];
+		}
+
+		nodes.reverse();
+		return nodes;
+	}
+
+	var findShortestPath = function (map, nodes) {
+		var start = nodes.shift(),
+		    end,
+		    predecessors,
+		    path = [],
+		    shortest;
+
+		while (nodes.length) {
+			end = nodes.shift();
+			predecessors = findPaths(map, start, end);
+
+			if (predecessors) {
+				shortest = extractShortest(predecessors, end);
+				if (nodes.length) {
+					path.push.apply(path, shortest.slice(0, -1));
+				} else {
+					return path.concat(shortest);
+				}
+			} else {
+				return null;
+			}
+
+			start = end;
+		}
+	}
+
+	var toArray = function (list, offset) {
+		try {
+			return Array.prototype.slice.call(list, offset);
+		} catch (e) {
+			var a = [];
+			for (var i = offset || 0, l = list.length; i < l; ++i) {
+				a.push(list[i]);
+			}
+			return a;
+		}
+	}
+
+	var Graph = function (map) {
+		this.map = map;
+	}
+
+	Graph.prototype.findShortestPath = function (start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(this.map, start);
+		} else if (arguments.length === 2) {
+			return findShortestPath(this.map, [start, end]);
+		} else {
+			return findShortestPath(this.map, toArray(arguments));
+		}
+	}
+
+	Graph.findShortestPath = function (map, start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(map, start);
+		} else if (arguments.length === 3) {
+			return findShortestPath(map, [start, end]);
+		} else {
+			return findShortestPath(map, toArray(arguments, 1));
+		}
+	}
+
+	return Graph;
+
+})();
+function distance(lat_a, lon_a, lat_b, lon_b)  { a = Math.PI / 180; lat1 = lat_a * a; lat2 = lat_b * a; lon1 = lon_a * a; lon2 = lon_b * a;  t1 = Math.sin(lat1) * Math.sin(lat2); t2 = Math.cos(lat1) * Math.cos(lat2); t3 = Math.cos(lon1 - lon2); t4 = t2 * t3; t5 = t1 + t4; rad_dist = Math.atan(-t5/Math.sqrt(-t5 * t5 +1)) + 2 * Math.atan(1);  return (rad_dist * 3437.74677 * 1.1508) * 1.6093470878864446; }
 var http = require('http');
 var express = require('express');
 var app = express();
@@ -9,11 +155,15 @@ var cookieParser = require('cookie-parser');
 var cookieParser = cookieParser('your secret sauce')
   , sessionStore = new session.MemoryStore();
 var session;
+var mysql = require('mysql');
 function extractFromAdress(components, type) {
   for (var i = 0; i < components.length; i++)
   for (var j = 0; j < components[i].types.length; j++)
   if (components[i].types[j] == type) return components[i].long_name;
   return "";
+}
+function getBestItineraire(itineraire, socket) {
+	socket.emit('succes', {itineraire:itineraire});
 }
 function check(post) {
 	var returnVar = new Array();
@@ -152,6 +302,72 @@ io.on('connection', function (socket) {
 		if(typeof checkVar === 'string') {
 			socket.emit('erreur', {titre:'Ooops...', content:checkVar});
 		} else {
+			var connection = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'emmanuelcoppey',
+			  password : 'azerty',
+			  port     : 8889,
+			  database : 'centrocar'
+			});
+			connection.query('SELECT * FROM arrets WHERE get_distance_metres("'+checkVar['de'][0]+'", "'+checkVar['de'][1]+'", lat, lng) < 20000 ORDER BY get_distance_metres("'+checkVar['de'][0]+'", "'+checkVar['de'][1]+'", lat, lng) ASC LIMIT 20', function(errDe, rowsDe, fieldsDe) {
+			  if(errDe || typeof rowsDe[0] == 'undefined') {
+			  	socket.emit('erreur', {titre:'Ooops...', content:'Aucun itinéraire n\'a été trouvé.'});
+			  } else {
+			  	connection.query('SELECT * FROM arrets WHERE get_distance_metres("'+checkVar['a'][0]+'", "'+checkVar['a'][1]+'", lat, lng) < 20000 ORDER BY get_distance_metres("'+checkVar['a'][0]+'", "'+checkVar['a'][1]+'", lat, lng) ASC LIMIT 20', function(errA, rowsA, fieldsA) {
+				  if(errA || typeof rowsA[0] == 'undefined') {
+				  	socket.emit('erreur', {titre:'Ooops...', content:'Aucun itinéraire n\'a été trouvé.'});
+				  } else {
+				  	console.log('Il y a des arrets a proximité.');
+				  	var i = 0;
+				  	var itineraire = [];
+				  	for(var keyDe in rowsDe) {
+				  		var nbrDe = rowsDe.length;
+				  		for(var keyA in rowsA) {
+				  			i++;
+				  			var nbrA = rowsA.length;
+				  			var totalCount = nbrA * nbrDe;
+				  			if(i == totalCount) getBestItineraire(itineraire, socket);
+						  	connection.query('SELECT * FROM arrets ORDER BY ligneId ASC,idArret ASC', function(errArret, rowsArret, fieldsArret) {
+						  		if(errArret || typeof rowsArret[0] == 'undefined') {
+						  			socket.emit('erreur', {titre:'Ooops...', content:'Aucun itinéraire n\'a été trouvé.'});
+						  		}
+						  		else {
+							  		var map = [];
+							  		var last;
+							  		for(var key in rowsArret){
+							  			var current = rowsArret[key];
+							  		    if(last) {
+							  		    	if(last.ligneId == current.ligneId) {
+							  		    		map[last.id] = [];
+							  		    		map[current.id] = [];
+							  		    		map[last.id][current.id] = distance(last.lat, last.lng, current.lat, current.lng)*1000;
+							  		    		map[current.id][last.id] = distance(last.lat, last.lng, current.lat, current.lng)*1000;
+							  		    	}
+							  		    }
+							  		    last = rowsArret[key];
+									}
+									map = JSON.parse(JSON.stringify(map));
+								  	//var map = {a:{b:3,c:1},b:{a:2,c:1},c:{a:4,b:1}},
+									var graph = new Graph(map);
+									itineraire.push(graph.findShortestPath(''+rowsDe[keyDe].id+'', ''+rowsA[keyA].id+''));
+									console.log(graph.findShortestPath(''+rowsDe[keyDe].id+'', ''+rowsA[keyA].id+''));      // => ['a', 'c', 'b']
+									/*graph.findShortestPath('a', 'c');      // => ['a', 'c']
+									graph.findShortestPath('b', 'a');      // => ['b', 'a']
+									graph.findShortestPath('b', 'c', 'b'); // => ['b', 'c', 'b']
+									graph.findShortestPath('c', 'a', 'b'); // => ['c', 'b', 'a', 'c', 'b']
+									graph.findShortestPath('c', 'b', 'a'); // => ['c', 'b', 'a']*/
+								}
+						  		if(errArret) console.log(errArret);
+						  	});
+						}
+					}
+				  }
+				  if(errA) console.log(errA);
+				});
+			  }
+			  if(errDe) console.log(errDe);
+			});
+			//connection.end();
 			socket.emit('succes', {itineraire:JSON.stringify(checkVar)});
 		}
 	});
